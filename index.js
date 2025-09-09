788eb8bc23d4f1cfb3996f471a93cfbc77a2c731
Stanley Barasa <sgbarasa@gmail.com>
2025-09-09 05:51:21 +0300

    complete project

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// index.js
 
-// Write your code here
+// 1. multiply should be an equation that equals 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 31 * 2 = 62
+
+// 2. random should be a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1; 
+
+// 3. mod should be an equation whose remainder is 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4; // 24 % 5 = 4
+
+// 4. max should be the highest number in a set
+const max = Math.max(1, 2, 3, 4, 20); // 20 is the largest
